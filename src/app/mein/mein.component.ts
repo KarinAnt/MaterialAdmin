7ed4d92b3e8d209e7b61ee9eb7955f0b1a976159
KarinAnt <antonyankarinka@gmail.com>
2019-10-23 18:14:07 +0400

    The end

--- src/app/mein/mein.component.ts
+++ src/app/mein/mein.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MeinComponent implements OnInit {
   @Input() menuPage :{name:string, iconName:string};
+  @Input() dasForm;
+  public form=false;
+  public das=false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  addClass(elem){
+    if(elem === 'Forms'){
+      this.form = true;
+      this.das = false;
+    }else if(elem === "Dashboard"){
+      this.form = false;
+      this.das = true;
+    }
+  }
 }
--- src/app/sidenav-autosize/sidenav-autosize.component.ts
+++ src/app/sidenav-autosize/sidenav-autosize.component.ts
@@ -8,38 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class SidenavAutosizeComponent implements OnInit {
   showFiller = true;
   public dasAndForm=true;
-  menuArray:{name:string, iconName:string}[]=[{
+  menuArray:{name:string, iconName:string, classSilver:string}[]=[{
     name: 'Dashboard',
-    iconName:'desktop_mac'
+    iconName:'desktop_mac',
+    classSilver:'silver'
   },
   {
     name: 'Forms',
-    iconName:'filter_tilt_shift'
+    iconName:'filter_tilt_shift',
+    classSilver:'silver'
   },
   {
     name: 'UI Features',
-    iconName:'dashboard'
+    iconName:'dashboard',
+    classSilver:''
   },
   {
     name: 'Tables',
-    iconName:'grid_on'
+    iconName:'grid_on',
+    classSilver:''
   },
   {
     name: 'Charts',
-    iconName:'pie_chart'
+    iconName:'pie_chart',
+    classSilver:''
   },
   {
     name: 'Sample Pages',
-    iconName:'pages'
+    iconName:'pages',
+    classSilver:''
   }];
   constructor() { }
 
   ngOnInit() {
   }
+
   dashboardAndForms(elem){
-    if(elem === 'Dashboard'){
+    if (elem === 'Dashboard') {
       this.dasAndForm = true;
-    }else if(elem === 'Forms'){
+    } else if(elem === 'Forms') {
       this.dasAndForm = false;
     }
   }
