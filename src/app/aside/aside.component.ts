e3814c084a8a92437b4510e10ffd175a1559b7e9
KarinAnt <antonyankarinka@gmail.com>
2019-10-23 18:44:52 +0400

    spineer

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -6,23 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aside.component.css']
 })
 export class AsideComponent implements OnInit {
-  ShowLoader = true;
   color = 'red';
   mode = 'determinate';
-  value = 50;
-  constructor() { 
-    // this.ShowLoader = true;
-    // var val = setInterval(function(){
-    //   this.ShowLoader = true;
-
-    //   this.value = 43 ;
-    //   console.log(this.value);
-    //   if(this.value > 40){
-    //     clearInterval(val)
-    //   }
-    // },1000)
+  value = 0;
+  
+  constructor() {
+    var val = setInterval(() => {
+      this.value++;
+      if(this.value > 40){
+        clearInterval(val)
+      }
+    },30)
   }
-
   ngOnInit() {
   }
 
